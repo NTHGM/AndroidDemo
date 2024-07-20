32a0bafc5d7be52ec679fef7fb1558e42c72ff71
huynt204567 <nongthanhhuy2002pl@gmail.com>
2024-07-20 07:28:05 +0700

    no message

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.todolist
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +13,40 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.ui.theme.TodoListTheme
 
 class MainActivity : ComponentActivity() {
+    public lateinit var adapter:ViewAdapter
+    lateinit var inputText : EditText
+    public lateinit var addBut:Button
+    public lateinit var deleteBut:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ViewAdapter(mutableListOf())
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        inputText = findViewById(R.id.input)
+        addBut = findViewById(R.id.addBut)
+        deleteBut = findViewById(R.id.deleteBut)
+
+        addBut.setOnClickListener{
+            val taskName = inputText.text.toString()
+            if(!taskName.trim().isEmpty()){
+                val task = Task(taskName)
+                adapter.addTask(task)
+            }
+            inputText.text.clear()
+        }
+
+        deleteBut.setOnClickListener{
+            adapter.deleteDoneTask()
+        }
     }
 }
---
+++ app/src/main/java/com/example/todolist/ViewAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.todolist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+
+class ViewAdapter(val taskList:MutableList<Task>):
+    RecyclerView.Adapter<ViewAdapter.TaskViewHolder>(){
+
+    class TaskViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+        val textView:TextView = itemView.findViewById(R.id.taskName)
+        val check:CheckBox = itemView.findViewById(R.id.checkBox)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val from = LayoutInflater.from(parent.context).inflate(R.layout.task,parent,false)
+        return TaskViewHolder(from)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val cur = taskList[position]
+        holder.apply {
+            textView.text = cur.taskInfo
+            check.isChecked = cur.isDone
+            check.setOnCheckedChangeListener{
+                    _, _ ->
+                cur.isDone =!cur.isDone
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return taskList.size;
+    }
+
+    fun addTask(task: Task) {
+        taskList.add(task)
+        notifyItemInserted(taskList.size - 1)
+    }
+
+    fun deleteDoneTask() {
+        taskList.removeAll { task ->
+            task.isDone
+        }
+        notifyDataSetChanged()
+    }
+}
